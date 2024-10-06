54c724b7504614154d23e7a03c5559c5b395b64b
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-05 23:14:33 -0400

    Updating assume role

--- assume_role.py
+++ assume_role.py
@@ -17,14 +17,11 @@ print(credentials)
 
 # print(response)
 
-
-response = boto3.create_security_group(
+ec2=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
+response = ec2.create_security_group(
     Description='My security group',
     GroupName='my-security-group',
-    VpcId='vpc-0c6460b8c3c8fe62f',
-    aws_access_key_id=credentials['AccessKeyId'],
-    aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['SessionToken']
+    VpcId='vpc-0c6460b8c3c8fe62f'
     )
 
 print(response)

02133b3c09762178a704744f4fd804f62b9b8796
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-06 04:16:21 -0400

    testing endpoint

--- deploy_clixx.py
+++ deploy_clixx.py
@@ -41,10 +41,124 @@ response = rds_client.restore_db_instance_from_db_snapshot(
 print(response)
 
 
-#Describing Database
-response = rds_client.describe_db_instances(DBInstanceIdentifier='wordpressdbclixx-ecs')
-endpoint = response['DBInstances'][0]['Endpoint']
-print(endpoint)
+
+AWS_REGION = "us-east-1"
+KEY_PAIR_NAME = 'Azeez10'
+AMI_ID = 'ami-00f251754ac5da7f0' # Amazon Linux 2
+SUBNET_ID = 'subnet-0ecd44e7315ae879d'
+SECURITY_GROUP_ID = 'sg-0fef030fc2befbb1e'
+#INSTANCE_PROFILE = 'CliXX'
+USER_DATA = '''#!/bin/bash
+yum update
+sudo yum install git -y
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+sudo yum install -y httpd mariadb-server
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl is-enabled httpd
+ 
+##Add ec2-user to Apache group and grant permissions to /var/www
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+cd /var/www/html
+ 
+#Install wordpress and unzip it/copy the sample php conf to wp-config
+##sudo wget https://wordpress.org/latest.tar.gz
+##sudo tar -xzf latest.tar.gz
+##cp wordpress/wp-config-sample.php wordpress/wp-config.php
+##start the mariadb and create a database/user and grant priv
+ 
+ if [ -f /var/www/html/wp-config.php ]
+ then
+    echo "wp-config.php already exists"
+    
+else
+    echo "wp-config.php does not exist"
+    git clone https://github.com/stackitgit/CliXX_Retail_Repository.git
+fi
+        
+
+
+#git clone https://github.com/stackitgit/CliXX_Retail_Repository.git
+cp -r CliXX_Retail_Repository/* /var/www/html
+ 
+## set Wordpress to run in an alternative directory
+#sudo mkdir /var/www/html/blog
+#sudo cp -r wordpress/* /var/www/html/
+ 
+## Allow wordpress to use Permalinks
+sudo sed -i '151s/None/All/' /etc/httpd/conf/httpd.conf
+sudo sed -i 's/wordpress-db.cc5iigzknvxd.us-east-1.rds.amazonaws.com/wordpressdbclixx-ecs.cn2yqqwoac4e.us-east-1.rds.amazonaws.com/' /var/www/html/wp-config.php
+if [ $? == 0 ]
+then
+    echo "sed was done"
+else
+    echo "sed was not done"
+fi
+
+
+##Grant file ownership of /var/www & its contents to apache user
+sudo chown -R apache /var/www
+ 
+##Grant group ownership of /var/www & contents to apache group
+sudo chgrp -R apache /var/www
+ 
+##Change directory permissions of /var/www & its subdir to add group write 
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} \;
+ 
+##Recursively change file permission of /var/www & subdir to add group write perm
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
+ 
+##Restart Apache
+sudo systemctl restart httpd
+sudo service httpd restart
+ 
+##Enable httpd 
+sudo systemctl enable httpd 
+sudo /sbin/sysctl -w net.ipv4.tcp_keepalive_time=200 net.ipv4.tcp_keepalive_intvl=200 net.ipv4.tcp_keepalive_probes=5
+'''
+EC2_RESOURCE = boto3.resource('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
+EC2_CLIENT = boto3.client('ec2', region_name=AWS_REGION)
+instances = EC2_RESOURCE.create_instances(
+    MinCount = 1,
+    MaxCount = 1,
+    ImageId=AMI_ID,
+    InstanceType='t2.micro',
+    KeyName=KEY_PAIR_NAME,
+    SecurityGroupIds = [SECURITY_GROUP_ID],
+    SubnetId=SUBNET_ID,
+    UserData=USER_DATA,
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'my-ec2-instance'
+                },
+            ]
+        },
+    ]
+)
+
+for instance in instances:
+    print(f'EC2 instance "{instance.id}" has been launched')
+    
+    instance.wait_until_running()
+    
+    # EC2_CLIENT.associate_iam_instance_profile(
+    #     IamInstanceProfile = {'Name': INSTANCE_PROFILE},
+    #     InstanceId = instance.id,
+    # )
+    # print(f'EC2 Instance Profile "{INSTANCE_PROFILE}" has been attached')
+    # print(f'EC2 instance "{instance.id}" has been started')
+
+
+
+
 
 
 

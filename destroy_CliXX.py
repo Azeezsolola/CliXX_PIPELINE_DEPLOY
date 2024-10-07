5e987f044c1e2d737ff2a1f3fdb6a8382c69f4a2
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-07 15:35:11 -0400

    Deleting instance and rds DB

--- destroy_CliXX.py
+++ destroy_CliXX.py
@@ -13,10 +13,29 @@ print(credentials)
 
 
 
+# ec2=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
+
+# response = ec2.delete_security_group(
+#     #GroupId='string',
+#     GroupName='my-security-group'
+#     )
+# print(response)
+
+
+
 ec2=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
 
-response = ec2.delete_security_group(
-    #GroupId='string',
-    GroupName='my-security-group'
-    )
-print(response)
+response = ec2.delete_instance(
+    InstanceId='i-093df4a3a15d7e3d3',
+    DeleteElasticIp=True,
+    DeleteVolumes=False
+)
+
+rds=boto3.client('rds',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
+
+response = client.delete_db_instance(
+    DBInstanceIdentifier='wordpressdbclixx-ecs',
+    SkipFinalSnapshot=False,
+    FinalDBSnapshotIdentifier='wordpressdbclixx-ecs-latest',
+    DeleteAutomatedBackups=False
+)

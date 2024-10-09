9139c30e2ddee07dcb97b4a3bdff3c0ef91dd300
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-09 11:41:04 -0400

    Pushing CliXX AUtoscaling py

---
+++ httpsclixx.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+import boto3,botocore,base64,time
+
+AWS_REGION='us-east-1'
+
+
+sts_client=boto3.client('sts')
+
+#Calling the assume_role function
+assumed_role_object=sts_client.assume_role(RoleArn='arn:aws:iam::495599767034:role/Engineer', RoleSessionName='mysession')
+
+credentials=assumed_role_object['Credentials']
+
+print(credentials)
+
+
+#Registering Domain Name 
+register_domain=boto3.client('route53',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
+response = register_domain.register_domain(
+    DomainName='codebuild-azeez.com',
+    
+    DurationInYears=1,
+    AutoRenew=False,
+    AdminContact={
+        'FirstName': 'Azeez',
+        'LastName': 'solola',
+        'ContactType': 'PERSON',
+        'OrganizationName': 'codebuild',
+        'AddressLine1': '8500 charnwood ct',
+        
+        'City': 'manassass',
+        'State': 'Virginia',
+        'CountryCode': 'VA',
+        'ZipCode': '20111',
+        'PhoneNumber': '2407964613',
+        'Email': 'ifeoluwsolola@gmail.com'},
+        
+
+    RegistrantContact={
+        'FirstName': 'Azeez',
+        'LastName': 'Solola',
+        'ContactType': 'PERSON',
+        'OrganizationName': 'codebuild',
+        'AddressLine1': '8500 charnwood ct',
+       
+        'City': 'manassas',
+        'State': 'virginia',
+        'CountryCode': 'VA',
+        'ZipCode': '20111',
+        'PhoneNumber': '2407964613',
+        'Email': 'ifeoluwsolola@gmail.com'},
+        
+
+    TechContact={
+        'FirstName': 'Azeez',
+        'LastName': 'solola',
+        'ContactType': 'PERSON',
+        'OrganizationName': 'codebuild',
+        'AddressLine1': '8500 charnwood ct',
+        
+        'City': 'manassas',
+        'State': 'virginia',
+        'CountryCode': 'VA',
+        'ZipCode': '20111',
+        'PhoneNumber': '2407964613',
+        'Email': 'ifeoluwsolola@gmail.com'},
+        
+
+    PrivacyProtectAdminContact=True,
+    PrivacyProtectRegistrantContact=True,
+    PrivacyProtectTechContact=True,
+    BillingContact={
+        'FirstName': 'Azeez',
+        'LastName': 'solola',
+        'ContactType': 'PERSON',
+        'OrganizationName': 'codebuild',
+        'AddressLine1': '8500 charnwood ct',
+ 
+        'City': 'manassas',
+        'State': 'virginia',
+        'CountryCode': 'VA',
+        'ZipCode': '20111',
+        'PhoneNumber': '2407964613',
+        'Email': 'ifeoluwasolola@gmail.com'},
+        
+
+    PrivacyProtectBillingContact=True
+)
+
+print(response)

f11179b867004deb27f799d2dea75f1344b4827e
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-11 23:09:42 -0400

    CliXX Autoscaling Tested Ok (Final)

--- destroy_CliXX.py
+++ destroy_CliXX.py
@@ -31,7 +31,7 @@ time.sleep(200)
 #Deleting Load Balancer
 elb=boto3.client('elbv2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = elb.delete_load_balancer(
-    LoadBalancerArn='arn:aws:elasticloadbalancing:us-east-1:495599767034:loadbalancer/app/autoscalinglb2-azeez/188e66b3c2e4ec62'
+    LoadBalancerArn='arn:aws:elasticloadbalancing:us-east-1:495599767034:loadbalancer/app/autoscalinglb2-azeez/76d2cb2eb46c9a69'
 )
 
 time.sleep(60)
@@ -39,22 +39,18 @@ time.sleep(60)
 #Deleting target group
 elb2=boto3.client('elbv2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = elb2.delete_target_group(
-    TargetGroupArn='arn:aws:elasticloadbalancing:us-east-1:495599767034:targetgroup/clixxautoscalingtg2/7a96b779197ef848'
+    TargetGroupArn='arn:aws:elasticloadbalancing:us-east-1:495599767034:targetgroup/clixxautoscalingtg2/e3f80e2826314ccf'
 )
 
 
-
+"""
 mounttarget=boto3.client('efs',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 for x in ["fsmt-08ffb5694c5808193","fsmt-0c477713965320c21","fsmt-025adc0191ce6773b","fsmt-0ab7d8ba37dead741","fsmt-0f3db873f897bf45b","fsmt-0923aab6b2be93810"]:
     response=mounttarget.delete_mount_target(
     MountTargetId=x)
+"""
 
 
-#Deleting File system
-efs=boto3.client('efs',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
-response = efs.delete_file_system(
-    FileSystemId='fs-06209f61a8979e1d9'
-)
 
 
 
@@ -65,11 +61,12 @@ response =autoscaling.delete_auto_scaling_group(
     ForceDelete=True
 )
 
+"""
 #Deleting File system
 efs=boto3.client('efs',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = efs.delete_file_system(
     FileSystemId='fs-06209f61a8979e1d9'
 )
-
+"""
 
 

0c0d6df0eafe95251b488c74ae73cc6fe9ff653f
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-11 22:12:03 -0400

    CliXX Autoscaling Tested Ok (Final)

--- destroy_CliXX.py
+++ destroy_CliXX.py
@@ -18,7 +18,7 @@ print(credentials)
 
 
 
-"""
+
 #Deleting RDS
 rds_client=boto3.client('rds',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = rds_client.delete_db_instance(
@@ -41,20 +41,20 @@ elb2=boto3.client('elbv2',aws_access_key_id=credentials['AccessKeyId'],aws_secre
 response = elb2.delete_target_group(
     TargetGroupArn='arn:aws:elasticloadbalancing:us-east-1:495599767034:targetgroup/clixxautoscalingtg2/7a96b779197ef848'
 )
-"""
 
 
-# mounttarget=boto3.client('efs',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
-# for x in ["fsmt-08ffb5694c5808193","fsmt-0c477713965320c21","fsmt-025adc0191ce6773b","fsmt-0ab7d8ba37dead741","fsmt-0f3db873f897bf45b","fsmt-0923aab6b2be93810"]:
-#     response=mounttarget.delete_mount_target(
-#     MountTargetId=x)
+
+mounttarget=boto3.client('efs',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
+for x in ["fsmt-08ffb5694c5808193","fsmt-0c477713965320c21","fsmt-025adc0191ce6773b","fsmt-0ab7d8ba37dead741","fsmt-0f3db873f897bf45b","fsmt-0923aab6b2be93810"]:
+    response=mounttarget.delete_mount_target(
+    MountTargetId=x)
 
 
-# #Deleting File system
-# efs=boto3.client('efs',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
-# response = efs.delete_file_system(
-#     FileSystemId='fs-06209f61a8979e1d9'
-# )
+#Deleting File system
+efs=boto3.client('efs',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
+response = efs.delete_file_system(
+    FileSystemId='fs-06209f61a8979e1d9'
+)
 
 
 

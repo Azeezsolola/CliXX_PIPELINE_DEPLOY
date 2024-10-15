7b916bfa4bac2c1d4a83351811d2405ba60f01aa
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-15 05:14:36 -0400

    Test

--- clixx_autoscaling.py
+++ clixx_autoscaling.py
@@ -167,6 +167,7 @@ print(response)
 
 
 
+
 #Creating NAT gateway
 NAT=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = NAT.create_nat_gateway(

a52dc21063013dc9f89d14bd17688da8eef6cc9d
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-16 04:12:13 -0400

    Test

--- destroy_CliXX.py
+++ destroy_CliXX.py
@@ -132,8 +132,8 @@ response = SG2.delete_security_group(
 LT=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = LT.delete_launch_template(
     DryRun=False,
-    LaunchTemplateId='lt-09b2a224d820f1645',
-    LaunchTemplateName='oloyede'
+    LaunchTemplateId='lt-09b2a224d820f1645'
+    
 )
 
 # #Deleting Internet Gateway

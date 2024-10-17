5748e07a66de81da65e19f57afb824cc6b5e505f
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-17 16:41:24 -0400

    Test

--- clixx_autoscaling.py
+++ clixx_autoscaling.py
@@ -720,46 +720,34 @@ for subnet_id in subnet_ids:
 
 
 
-FILE_SYSTEM_ID=response["FileSystemId"]
+FILE=response["FileSystemId"]
 MOUNT_POINT="/var/www/html"
 REGION='us-east-1'
-DNS='https://dev.clixx-azeez.com'
-EFS_DNS="{FILE_SYSTEM_ID}.efs.{AWS_REGION}.amazonaws.com"
+lb_dns='https://dev.clixx-azeez.com'
+
 
 #Creating Launch Template 
 
 AWS_REGION='us-east-1'
-USER_DATA = f"""#!/bin/bash
-exec > /var/log/userdata.log 2>&1  # Log output to a file
-echo "Starting userdata script..."
+USER_DATA = """#!/bin/bash
 
-# Install necessary packages
+##Install the needed packages and enable the services(MariaDb, Apache)
 sudo yum update -y
-sudo yum install -y nfs-utils git
 
-# Mounting configuration
-MOUNT_POINT="/var/www/html"
-EFS_DNS="{FILE_SYSTEM_ID}.efs.{AWS_REGION}.amazonaws.com"
+#Get Ipaddress
+#IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 
-# Create mount point
+#Mounting 
+sudo yum install -y nfs-utils
+
+#TOKEN=$(curl --request PUT "http://169.254.169.254/latest/api/token" --header "X-aws-ec2-metadata-token-ttl-seconds: 3600")
+#REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region --header "X-aws-ec2-metadata-token: $TOKEN")
+#MOUNT_POINT="/var/www/html"
 sudo mkdir -p ${MOUNT_POINT}
 sudo chown ec2-user:ec2-user ${MOUNT_POINT}
+echo "${FILE}.efs.${REGION}.amazonaws.com:/ ${MOUNT_POINT} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev 0 0" | sudo tee -a /etc/fstab
+sudo mount -a 
 
-# Add EFS to fstab
-echo "${EFS_DNS}:/ ${MOUNT_POINT} nfs4 defaults,_netdev 0 0" | sudo tee -a /etc/fstab
-
-# Wait for the network to stabilize
-sleep 30
-
-# Attempt to mount
-if sudo mount -a; then
-    echo "Mount successful"
-else
-    echo "Mount failed"
-    
-fi
-sudo chmod -R 755 /var/www/html
-sudo mount -a
 sudo yum install git -y
 sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 sudo yum install -y httpd mariadb-server
@@ -770,18 +758,18 @@ sudo systemctl is-enabled httpd
 ##Add ec2-user to Apache group and grant permissions to /var/www
 sudo usermod -a -G apache ec2-user
 sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {{}} \;
-find /var/www -type f -exec sudo chmod 0664 {{}} \;
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
 cd /var/www/html
- 
+
 #Install wordpress and unzip it/copy the sample php conf to wp-config
 ##sudo wget https://wordpress.org/latest.tar.gz
 ##sudo tar -xzf latest.tar.gz
 ##cp wordpress/wp-config-sample.php wordpress/wp-config.php
 ##start the mariadb and create a database/user and grant priv
- 
- if [ -f /var/www/html/wp-config.php ]
- then
+
+if [ -f /var/www/html/wp-config.php ]
+then
     echo "wp-config.php already exists"
     
 else
@@ -793,14 +781,16 @@ fi
 
 #git clone https://github.com/stackitgit/CliXX_Retail_Repository.git
 cp -r CliXX_Retail_Repository/* /var/www/html
- 
+
 ## set Wordpress to run in an alternative directory
 #sudo mkdir /var/www/html/blog
 #sudo cp -r wordpress/* /var/www/html/
- 
+
+
 ## Allow wordpress to use Permalinks
 sudo sed -i '151s/None/All/' /etc/httpd/conf/httpd.conf
-sudo sed -i 's/wordpress-db.cc5iigzknvxd.us-east-1.rds.amazonaws.com/wordpressdbclixx-ecs2.cn2yqqwoac4e.us-east-1.rds.amazonaws.com/' /var/www/html/wp-config.php
+sudo sed -i 's/wordpress-db.cc5iigzknvxd.us-east-1.rds.amazonaws.com/wordpressdbclixx-ecs.cn2yqqwoac4e.us-east-1.rds.amazonaws.com/' /var/www/html/wp-config.php
+
 if [ $? == 0 ]
 then
     echo "sed was done"
@@ -808,45 +798,51 @@ else
     echo "sed was not done"
 fi
 
-#DNS=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
-DNS='https://dev.clixx-azeez.com'
-echo $DNS
 
-output_variable=$(mysql -u wordpressuser -p -h wordpressdbclixx-ecs2.cn2yqqwoac4e.us-east-1.rds.amazonaws.com -D wordpressdb -p'W3lcome123' -sse "select option_value from wp_options where option_value like 'CliXX-APP-%';")
+
+#DNS=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
+#echo $DNS
+#DNS=${lb_dns}
+sleep 600
+output_variable=$(mysql -u wordpressuser -p -h wordpressdbclixx-ecs.cn2yqqwoac4e.us-east-1.rds.amazonaws.com -D wordpressdb -pW3lcome123 -sse "select option_value from wp_options where option_value like 'CliXX-APP-%';")
 echo $output_variable
 
-if [ output_variable == ${DNS} ]
+if [ output_variable == ${lb_dns} ]
 then
     echo "DNS Address in the the table"
 else
     echo "DNS Address is not in the table"
     #Logging DB
-    mysql -u wordpressuser -p -h wordpressdbclixx-ecs2.cn2yqqwoac4e.us-east-1.rds.amazonaws.com -D wordpressdb -pW3lcome123<<EOF
-    UPDATE wp_options SET option_value ='${DNS}' WHERE option_value LIKE 'CliXX-APP-%';
+    mysql -u wordpressuser -p -h wordpressdbclixx-ecs.cn2yqqwoac4e.us-east-1.rds.amazonaws.com -D wordpressdb -pW3lcome123<<EOF
+    UPDATE wp_options SET option_value ="${lb_dns}" WHERE option_value LIKE "CliXX-APP-%";
 EOF
 fi
 
+
 ##Grant file ownership of /var/www & its contents to apache user
 sudo chown -R apache /var/www
- 
+
 ##Grant group ownership of /var/www & contents to apache group
 sudo chgrp -R apache /var/www
- 
+
 ##Change directory permissions of /var/www & its subdir to add group write 
 sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {{}} \;
- 
+find /var/www -type d -exec sudo chmod 2775 {} \;
+
 ##Recursively change file permission of /var/www & subdir to add group write perm
-sudo find /var/www -type f -exec sudo chmod 0664 {{}} \;
- 
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
+
 ##Restart Apache
 sudo systemctl restart httpd
 sudo service httpd restart
- 
+
 ##Enable httpd 
 sudo systemctl enable httpd 
 sudo /sbin/sysctl -w net.ipv4.tcp_keepalive_time=200 net.ipv4.tcp_keepalive_intvl=200 net.ipv4.tcp_keepalive_probes=5
+
 """
+USER_DATA = USER_DATA.format(file=FILE, region=REGION, mount_point=MOUNT_POINT, lb_dns=lb_dns)
+
 encoded_user_data = base64.b64encode(USER_DATA.encode('utf-8')).decode('utf-8')
 
 ec2=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)

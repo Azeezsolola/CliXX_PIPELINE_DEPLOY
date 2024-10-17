eb718bb1c71ab004077d5d03af62a95d647fcd25
Azeezsolola <oloyedeifeoluwa21@gmail.com>
2024-10-16 21:07:15 -0400

    Test

--- clixx_autoscaling.py
+++ clixx_autoscaling.py
@@ -534,6 +534,7 @@ print(publicsubnetid2)
 
 
 
+
 #Associating route table to newly created public subnet  
 igwass5=boto3.client('ec2',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'],region_name=AWS_REGION)
 response = igwass5.associate_route_table(
